2f7b761185bf06c41b7ff84b81ecd2e95e411ebb
Ali <mitali777770@gmail.com>
2026-09-11 01:44:05 +0900

    feat: meta tags integration on document private page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo" />
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="" />
+
+        {/* SEO - Search engine Optimization */}
+        <meta name="keyword" content={"nestar, nestar.uz, devex mern, mern nestjs, mern nestjs fullstack"} />
+        <meta name="description" content={"Buy and sale properties anywhere anytime in South Korea."} />
+      </Head>
       <body>
         <Main />
         <NextScript />
