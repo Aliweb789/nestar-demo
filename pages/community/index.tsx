4878eae05dfd7391a072f94005277513d427a8ad
Ali <mitali777770@gmail.com>
2026-09-11 01:56:54 +0900

    feat: integration header and footer via layouts

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,5 +1,9 @@
-const Coommunity = () => {
+
+import withLayoutBasic from "@/libs/component/layout/LayoutHome";
+import { NextPage } from "next";
+
+const Coommunity: NextPage = () => {
   return <div>COMMUNITY</div>
 }
 
-export default Coommunity;
+export default withLayoutBasic(Coommunity);
